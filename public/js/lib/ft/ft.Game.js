64e47d7a46499da4ed1d802973219e7e98730bd6
Chris Rittelmeyer <chris.rittelmeyer@brickstream.com>
2013-06-03 18:32:05 -0400

    Dungeon City: Renamed ft.Game to ft.Dungeon since that is more accurate and created a new ft.Game which includes ft.GPS and ft.Dungeon libraries

--- public/js/lib/ft/ft.Game.js
+++ public/js/lib/ft/ft.Game.js
@@ -5,7 +5,7 @@
  */
 var ft = ft || {};
 
-define(["jquery", "ft/ft.MapGen", "ft/ft.Entity", "ft/ft.Utilities", "ft/ft.UI"], function($) {
+define(["jquery", "ft/ft.Utilities", "ft/ft.UI", "ft/ft.Dungeon", "ft/ft.GPS"], function($) {
 	var s, els;
 
 	ft.Game = (function() {
@@ -23,76 +23,11 @@ define(["jquery", "ft/ft.MapGen", "ft/ft.Entity", "ft/ft.Utilities", "ft/ft.UI"]
 
 			ft.UI.showLoader(els.$canvas);
 
-			_initMap(function() {
-				_initPlayer();
-			});
+			_initMap();
 		}
 
-		function _initMap(callback) {
-			setTimeout(function() {
-				ft.MapGen.init('Cellular Automata', {
-					generations: 4,
-					width: s.mapWidth,
-		    	height: s.mapHeight
-		    }, function(map) {
-					s.map = map;
-
-					_drawMap();
-
-					if (callback) callback.call();
-				});
-			}, 300);
-		}
-
-		function _drawMap() {
-			//clear canvas
-			els.$canvas.empty();
-
-			//redraw map from map array
-			$.each(s.map.getFlattenedMap(), function(i, column) {
-				els.$canvas.append(column.join("") + '<br>');
-			});
-		}
-
-		function _initPlayer() {
-			var options = _getRandomBlankLocation();
-			var player = ft.Entity.Player.init(options.x, options.y);
-
-			ft.Entity.Player.draw(s.map, player);
-			_drawMap();
-
-			ft.Utilities.subscribe('move.N', function() {
-				ft.Entity.Player.move(s.map, player, 'N');
-				_drawMap();
-			});
-			ft.Utilities.subscribe('move.E', function() {
-				ft.Entity.Player.move(s.map, player, 'E');
-				_drawMap();
-			});
-			ft.Utilities.subscribe('move.S', function() {
-				ft.Entity.Player.move(s.map, player, 'S');
-				_drawMap();
-			});
-			ft.Utilities.subscribe('move.W', function() {
-				ft.Entity.Player.move(s.map, player, 'W');
-				_drawMap();
-			});
-
-			ft.UI.hideLoader(els.$canvas);
-		}
-
-		function _getRandomBlankLocation() {
-			var mapArray = s.map.getMapArray();
-			var rand1 = ft.Utilities.getRandomNum(mapArray.length);
-			var rand2 = ft.Utilities.getRandomNum(mapArray[rand1].length);
-			if (ft.MapGen.tiles.ground.indexOf(mapArray[rand1][rand2].tile) > -1) {
-				return {
-					x: rand1,
-					y: rand2
-				};
-			} else {
-				return _getRandomBlankLocation();
-			}
+		function _initMap() {
+			
 		}
 
 		return {
